0f6767133c9529ff2200407e616896f1f66624c5
Sadda Hussain <SaddaHussain@AppsGenii.eu>
2018-02-22 12:45:28 +0500

    Ads Removed

--- app/src/main/java/pk/encodersolutions/findmyphone/MainActivity.kt
+++ app/src/main/java/pk/encodersolutions/findmyphone/MainActivity.kt
@@ -4,18 +4,12 @@ import android.Manifest
 import android.content.Context
 import android.content.SharedPreferences
 import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.SwitchCompat
 import android.widget.Toast
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.AdView
-import com.google.android.gms.ads.InterstitialAd
-import com.google.android.gms.ads.MobileAds
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,26 +17,13 @@ class MainActivity : AppCompatActivity() {
     private lateinit var editor: SharedPreferences.Editor
     private lateinit var switchNormalMode: SwitchCompat
     private lateinit var switchTurnAlarm: SwitchCompat
-    private lateinit var mInterstitialAd : InterstitialAd
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        initAds()
         initViews()
         requestPermissions()
         registerListeners()
-    }
-
-    private fun initAds() {
-        MobileAds.initialize(this, "ca-app-pub-6728394946634137~6666637867")
-        mInterstitialAd = InterstitialAd(this)
-        mInterstitialAd.adUnitId = "ca-app-pub-6728394946634137/5597098770"
-        mInterstitialAd.loadAd(AdRequest.Builder().build())
-        val mAdView: AdView = findViewById(R.id.adView)
-        val adRequest = AdRequest.Builder().build()
-        mAdView.loadAd(adRequest)
     }
 
     private fun initViews() {
@@ -64,15 +45,13 @@ class MainActivity : AppCompatActivity() {
     private fun requestPermissions() {
 
         if (ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_SMS),1)
-            }
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_SMS), 1)
+        } else if (ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
 
-        else if (ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECEIVE_SMS),2)
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECEIVE_SMS), 2)
         }
 
-        }
+    }
 
     private fun registerListeners() {
         switchNormalMode.setOnCheckedChangeListener { buttonView, isChecked ->
@@ -98,14 +77,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun showInterstitialAd(){
-        if (mInterstitialAd.isLoaded) {
-            mInterstitialAd.show()
-        }
-    }
 
     override fun onBackPressed() {
-        showInterstitialAd()
         super.onBackPressed()
     }
 
